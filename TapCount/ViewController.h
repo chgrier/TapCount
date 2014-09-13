5c2ed1fd4593b14e551f3083ccd50001838d7d86
Charles Grier <chgrier@mac.com>
2014-09-13 07:11:25 -0700

    Completed first screen with voice synthesis, controls, basic interface.

--- TapCount/ViewController.h
+++ TapCount/ViewController.h
@@ -7,7 +7,34 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioToolbox.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIAlertViewDelegate>
+{
+    NSInteger total;
+    NSInteger totalTwo;
+    IBOutlet UILabel *screen;
+    IBOutlet UILabel *screenTwo;
+}
+
+- (IBAction)increment:(id)sender;
+- (IBAction)decrement:(id)sender;
+
+- (IBAction)incrementTwo:(id)sender;
+- (IBAction)decrementTwo:(id)sender;
+
+- (IBAction)vibrateSwitch:(id)sender;
+- (IBAction)speechSwitch:(id)sender;
+- (IBAction)soundsOff:(id)sender;
+
+- (IBAction)sayTotal:(id)sender;
+
+
+
+@property (weak, nonatomic) IBOutlet UISwitch *speechSwitchToggle;
+@property (weak, nonatomic) IBOutlet UISwitch *vibrateSwithToggle;
+@property (weak, nonatomic) IBOutlet UISwitch *soundsSwitchToggle;
+
+@property (assign) SystemSoundID pewPewSound;
 
 @end
--- TapCount/ViewController.m
+++ TapCount/ViewController.m
@@ -7,23 +7,375 @@
 //
 
 #import "ViewController.h"
+#import <AudioToolbox/AudioToolbox.h>
+
+@import AVFoundation;
 
 @interface ViewController ()
 
+
+
 @end
 
+
 @implementation ViewController
+{
+    BOOL _vibrate;
+    BOOL _soundOn;
+    BOOL _speechOn;
+    
+    SystemSoundID _soundID;
+    
+    AVSpeechSynthesizer *_speechSynthesizer;
+    
+}
+- (void)viewWillAppear:(BOOL)animated
+{
+   
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+   // _vibrate = YES;
+    
+     _speechSynthesizer = [[AVSpeechSynthesizer alloc]init];
+    
+    _vibrate = YES;
+    _speechOn = YES;
+    _soundOn = YES;
+    
+    
+    AVSpeechUtterance *utterance = [[AVSpeechUtterance alloc]initWithString:@"Ready"];
+     [_speechSynthesizer speakUtterance:utterance];
+    
+    [self loadSoundEffect];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    total = 0;
 }
+
+
+
+
+- (BOOL)stopSpeakingAtBoundary:(AVSpeechBoundary)boundary{
+    
+    return YES;
+}
+
+
+-(IBAction)increment:(id)sender
+{
+    total++;
+    [screen setText:[NSString stringWithFormat:@"%ld", (long)total]];
+    
+    [self updateTextWithAnimation];
+    
+    NSString *count = [NSString stringWithFormat:@"%ld",(long)total];
+    
+    AVSpeechUtterance *utterance = [[AVSpeechUtterance alloc]initWithString:count];
+    
+    // *** class method alternative ***
+    //[_speechSynthesizer speakUtterance:[AVSpeechUtterance speechUtteranceWithString:count]];
+    
+    if (_speechOn == YES) {
+        
+    utterance.rate = AVSpeechUtteranceMinimumSpeechRate + ((AVSpeechUtteranceMaximumSpeechRate - AVSpeechUtteranceMinimumSpeechRate) * 0.5f);
+    utterance.volume = 1.0f;
+    utterance.pitchMultiplier = 1.2f;
+    utterance.postUtteranceDelay = 0.0;
+    utterance.voice = [AVSpeechSynthesisVoice voiceWithLanguage:@"en-us"];
+    
+    [_speechSynthesizer stopSpeakingAtBoundary:AVSpeechBoundaryImmediate];
+    
+    [_speechSynthesizer speakUtterance:utterance];
+    }
+    
+    if (_vibrate == YES) {
+        AudioServicesPlayAlertSound (kSystemSoundID_Vibrate);
+    }
+    
+    if (_soundOn == YES) {
+        
+        int a;
+        a = total;
+        int b;
+        b = 10;
+       
+        
+        if ( a % b == 0){
+        AudioServicesPlayAlertSound (kSystemSoundID_Vibrate);
+        
+        NSString *count = [NSString stringWithFormat:@"%ld Blasts",(long)total];
+        //NSString *count = [NSString stringWithFormat:@"Blasts"];
+
+        
+        AVSpeechUtterance *utterance = [[AVSpeechUtterance alloc]initWithString:count];
+        
+        [_speechSynthesizer speakUtterance:utterance];
+    }
+    }
+    
+   
+}
+
+-(IBAction)incrementTwo:(id)sender
+{
+    totalTwo++;
+    [screenTwo setText:[NSString stringWithFormat:@"%ld", (long)totalTwo]];
+    
+    [self updateTextWithAnimation];
+    
+    NSString *count = [NSString stringWithFormat:@"%ld",(long)totalTwo];
+    
+    AVSpeechUtterance *utterance = [[AVSpeechUtterance alloc]initWithString:count];
+    
+    // *** class method alternative ***
+    //[_speechSynthesizer speakUtterance:[AVSpeechUtterance speechUtteranceWithString:count]];
+    
+    if (_speechOn == YES) {
+        
+        utterance.rate = AVSpeechUtteranceMinimumSpeechRate + ((AVSpeechUtteranceMaximumSpeechRate - AVSpeechUtteranceMinimumSpeechRate) * 0.5f);
+        utterance.volume = 1.0f;
+        utterance.pitchMultiplier = 1.2f;
+        utterance.postUtteranceDelay = 0.0;
+        utterance.voice = [AVSpeechSynthesisVoice voiceWithLanguage:@"en-gb"];
+        
+        [_speechSynthesizer stopSpeakingAtBoundary:AVSpeechBoundaryImmediate];
+        
+        [_speechSynthesizer speakUtterance:utterance];
+        
+    
+        if (total == 52 && totalTwo == 20) {
+            [_speechSynthesizer speakUtterance:[AVSpeechUtterance speechUtteranceWithString:@"Go GATORS!"]];
+        }
+    }
+    
+    if (_vibrate == YES) {
+        AudioServicesPlayAlertSound (kSystemSoundID_Vibrate);
+    }
+    
+    if (_soundOn == YES) {
+        // alert when reach 10s
+        int a;
+        a = totalTwo;
+        int b;
+        b = 10;
+        if ( a % b == 0) {
+            AudioServicesPlayAlertSound (kSystemSoundID_Vibrate);
+            
+            NSString *count = [NSString stringWithFormat:@"%ld Other",(long)totalTwo];
+            //NSString *count = [NSString stringWithFormat:@"Other"];
+            
+            AVSpeechUtterance *utterance = [[AVSpeechUtterance alloc]initWithString:count];
+            
+            [_speechSynthesizer speakUtterance:utterance];
+        }
+    }
+    
+}
+
+
+
+-(IBAction)decrement:(id)sender
+{
+    total--;
+    [screen setText:[NSString stringWithFormat:@"%ld", (long)total]];
+    
+    if (_soundOn == YES) {
+        [self playSoundEffect];
+    }
+   
+}
+
+-(IBAction)decrementTwo:(id)sender
+{
+    totalTwo--;
+    [screenTwo setText:[NSString stringWithFormat:@"%ld", (long)totalTwo]];
+}
+
+// alert when tap clear button. If Yes, then go to didDismissWithButtonIndex method
+-(IBAction)clear:(id)sender
+{
+    
+    if (_speechOn == YES) {
+    [_speechSynthesizer speakUtterance:[AVSpeechUtterance speechUtteranceWithString:@"Hey Path Guy, Do you really want to clear the total?"]];
+    }
+    
+    UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"Clear Total?"
+                                                       message:@"Do you really want to clear the total?"
+                                                      delegate:self cancelButtonTitle:@"Yes"
+                                             otherButtonTitles:@"Cancel", nil];
+    
+    
+    [alertView show];
+
+}
+
+
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    // first button clears total
+    if (buttonIndex == 0)
+    {
+        [self clearTotal];
+        
+    // other buttons just return and data is not cleared
+    } else {
+        return;
+    }
+    
+}
+
+
+
+-(void) clearTotal
+{
+    total = 0;
+    totalTwo = 0;
+    [screen setText:[NSString stringWithFormat:@"%ld", (long)total]];
+    [screenTwo setText:[NSString stringWithFormat:@"%ld", (long)totalTwo]];
+    
+    if (_speechOn == YES) {
+        [_speechSynthesizer speakUtterance:[AVSpeechUtterance speechUtteranceWithString:@"Okay, you're the boss.  Just don't say I didn't warn you!"]];
+    }
+
+    
+}
+
+
+// UISWITCH CONTROLES
+
+-(IBAction)vibrateSwitch:(id)sender
+{
+    if ([_vibrateSwithToggle isOn]) {
+        _vibrate = YES;
+        
+        
+    } else {
+        _vibrate = NO;
+        
+        if (![_speechSwitchToggle isOn])
+        {
+            [_soundsSwitchToggle setOn:NO];
+            [UIView setAnimationDuration:0.8];
+        }
+        
+    }
+}
+
+- (IBAction)speechSwitch:(id)sender{
+    
+    if ([_speechSwitchToggle isOn]) {
+        _speechOn = YES;
+        [_soundsSwitchToggle setOn:YES];
+    } else {
+        _speechOn = NO;
+        
+        if (![_vibrateSwithToggle isOn])
+        {
+            [_soundsSwitchToggle setOn:NO];
+            
+        }
+    }
+}
+
+// cast sender as UISwitch (rather than generic id) to create condition
+- (IBAction)soundsOff:(UISwitch *)sender
+{
+    if (sender.on){
+        _vibrate = YES;
+        _speechOn = YES;
+        
+        [_speechSwitchToggle setOn:YES];
+        [_vibrateSwithToggle setOn:YES];
+
+    } else {
+    
+    _vibrate = NO;
+    _speechOn = NO;
+    
+    [_speechSwitchToggle setOn:NO];
+    [_vibrateSwithToggle setOn:NO];
+    
+}
+
+}
+
+- (IBAction)sayTotal:(UIButton *)totalButton {
+    if (_speechOn == YES) {
+       
+        if (totalButton.tag == 100) {
+            NSString *count = [NSString stringWithFormat:@"Blasts TOTAL IS %ld",(long)total];
+            AVSpeechUtterance *utterance = [[AVSpeechUtterance alloc]initWithString:count];
+        
+            [_speechSynthesizer speakUtterance:utterance];
+        }
+        
+        if (totalButton.tag == 101) {
+                NSString *count = [NSString stringWithFormat:@"Other TOTAL IS %ld",(long)totalTwo];
+                AVSpeechUtterance *utterance = [[AVSpeechUtterance alloc]initWithString:count];
+                utterance.voice = [AVSpeechSynthesisVoice voiceWithLanguage:@"en-gb"];
+                [_speechSynthesizer speakUtterance:utterance];
+            }
+
+    }
+    
+}
+
+- (void) updateTextWithAnimation
+{
+    
+    CATransition *transition = [CATransition animation];
+    transition.type = kCATransitionFade;
+    transition.subtype = kCATransitionFromTop;
+    
+    transition.duration = .2;
+    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+    
+    
+    [screen.layer addAnimation:transition forKey:nil];
+}
+
+#pragma mark - Sounds Effects
+
+- (void)loadSoundEffect
+{
+    NSString *path = [[NSBundle mainBundle]pathForResource:@"Sound.caf" ofType:nil];
+    
+    NSURL *fileURL = [NSURL fileURLWithPath:path isDirectory:NO];
+    if (fileURL == nil) {
+        NSLog(@"NSURL is nil");
+        return;
+    }
+    OSStatus error = AudioServicesCreateSystemSoundID((__bridge CFURLRef)fileURL, &_soundID);
+    
+    if (error != kAudioServicesNoError) {
+        NSLog(@"Error code %ld loading sound at path: %@", error, path);
+        
+        return;
+    }
+}
+
+- (void) unloadSoundEffect
+{
+    AudioServicesDisposeSystemSoundID(_soundID);
+    _soundID = 0;
+}
+
+- (void)playSoundEffect
+{
+    AudioServicesPlaySystemSound(_soundID);
+}
+
+
+
 
 @end
